612aa1ea047504a163efb906b1dee784ca3e8738
motofi <jessicamallred@gmail.com>
2020-05-23 15:00:52 -0400

    Files for initial ETL ER Diagram, postgreSQL schema, jupyter notebook for initial data load and transformation, and the ETL project report.

---
+++ etl-project/etl-project/etl_postgres_schema.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS suicides CASCADE;
+DROP TABLE IF EXISTS happiness CASCADE;
+
+CREATE TABLE "suicides" (
+    "Country" VARCHAR(50)   NOT NULL,
+    "Year" INT   NOT NULL,
+    "number_of_suicides" INT   NOT NULL,
+    "population" INT   NOT NULL,
+    CONSTRAINT "pk_suicides" PRIMARY KEY (
+        "Country","Year"
+     )
+);
+
+CREATE TABLE "happiness" (
+    "Country" VARCHAR(50)   NOT NULL,
+    "Year" INT   NOT NULL,
+    "Freedom" FLOAT   NOT NULL,
+    "GDP_per_capita" FLOAT   NOT NULL,
+    "Generosity" FLOAT   NOT NULL,
+    "Healthy_life_expectancy" FLOAT   NOT NULL,
+    "Happiness_Rank" INT   NOT NULL,
+    "Perceptions_of_corruption" FLOAT   NOT NULL,
+    "Happiness_Score" FLOAT   NOT NULL,
+    "Social_support" FLOAT   NOT NULL,
+    "Dystopia_Residual" FLOAT   NOT NULL,
+    "Whisker_high" FLOAT   NOT NULL,
+    "Whisker_low" FLOAT   NOT NULL,
+    "Family" FLOAT   NOT NULL,
+    "Region" VARCHAR(50)   NOT NULL,
+    "Upper_Confidence_Interval" FLOAT   NOT NULL,
+    "Lower_Confidence_Interval" FLOAT   NOT NULL,
+    "Standard_Error" FLOAT   NOT NULL,
+    CONSTRAINT "pk_happiness" PRIMARY KEY (
+        "Country","Year"
+     )
+);
